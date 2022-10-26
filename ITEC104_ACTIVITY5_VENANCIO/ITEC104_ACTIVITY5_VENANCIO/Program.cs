98f0ad91a255c7e0d26d77e69109f266ad9c41bd
Venancio, Nicko A <106007314+N1ckot1ne@users.noreply.github.com>
2022-10-27 00:49:24 +0800

    Add files via upload

---
+++ ITEC104_ACTIVITY5_VENANCIO/ITEC104_ACTIVITY5_VENANCIO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ITEC104_ACTIVITY5_VENANCIO
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> mylist = test(new List<int>(new int[] { 10, 20, 35, 65, 48, 53, 5, 1 }));
+
+            Console.WriteLine("10, 20, 35, 65, 48, 53, 5, 1");
+
+            foreach (var list in mylist)
+            {
+                Console.Write(list + " ");
+            }
+            Console.ReadLine();
+        }
+        public static List<int> test(List<int> nums)
+        {
+            return nums.Where(n => n % 10 != 5).ToList();
+        }
+
+        
+    }
+    
+}
